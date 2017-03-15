6880c70e7c49f840de5675b5aa9b0d3db93c0a64
Stephen Klos <stekloweb@gmail.com>
2017-03-14 22:49:21 -0400

    major portfolio updates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-// Main.js
+/* Main.js */
 
-var $navModal = $('.navModal');
-var $hamburger = $('.hamburger');
-var $logo = $('.navbar-logo');
 var $body = $('body');
+var $logo = $('.navbar-logo');
+var $hamburger = $('.hamburger');
+var $navModal = $('.navModal');
+var $webWorkItem = $('.web-work-item');
+var $webWorkModal = $('.web-work-modal');
+var $webWorkClose = $('.web-work-modal-close');
 
-function showOrHideModal() {
+
+/*
+------------------------------------------------------------------------------------
+Show or Hide Navigation Modal (Mobile Only)
+------------------------------------------------------------------------------------
+ */
+function showOrHideNavModal() {
     if ($navModal.hasClass('show')) {
         $body.removeClass('modal-open');
         $navModal.removeClass('fade-in'); 
@@ -22,7 +31,13 @@ function showOrHideModal() {
     }
 }
 
-function hideModal() {
+
+/*
+------------------------------------------------------------------------------------
+Hide Navigation Modal (Mobile Only)
+------------------------------------------------------------------------------------
+ */
+function hideNavModal() {
     if ($navModal.hasClass('show')) {
         $body.removeClass('modal-open');
         $navModal.removeClass('fade-in'); 
@@ -32,54 +47,121 @@ function hideModal() {
     }
 }
 
-function inactiveHamburger() {
-    if ($hamburger.hasClass('is-active')) {
-        $hamburger.removeClass('is-active');
-    }
-}
 
+/*
+------------------------------------------------------------------------------------
+Open or close mobile nav and toggle "is-active" class on hamburger menu 
+when menu item is clicked
+------------------------------------------------------------------------------------
+ */
 $('.navModal li a').on('click', function(e) {
     e.preventDefault();
-    showOrHideModal();
+    showOrHideNavModal();
     $hamburger.toggleClass('is-active');
     
 });
 
+
+/*
+------------------------------------------------------------------------------------
+Toggle "is-active" class on hamburger menu
+------------------------------------------------------------------------------------
+ */
 $hamburger.on('click', function(e) {
     e.preventDefault();
     $hamburger.toggleClass('is-active');
-    showOrHideModal();
+    showOrHideNavModal();
 });
 
+
+/*
+------------------------------------------------------------------------------------
+Hide mobile nav and deactivate hamburger menu when home button is clicked
+------------------------------------------------------------------------------------
+ */
 $logo.on('click', function(e) {
+    var $this = $(this);
     e.preventDefault();
-    inactiveHamburger();
-    hideModal();
+
+    if ($hamburger.hasClass('is-active')) {
+        $hamburger.removeClass('is-active');
+    }
+
+    hideNavModal();
 });
 
 
+/*
+------------------------------------------------------------------------------------
+Show or Hide Web Work Modal
+------------------------------------------------------------------------------------
+ */
+$webWorkItem.on('click', function(e) {
+    var $this = $(this);
+    var $next = $this.next();
+
+    e.preventDefault();
+
+    if ($next.hasClass('show')) {
+        $body.removeClass('modal-open');
+        $next.removeClass('fade-in'); 
+        setTimeout(function(){ 
+            $next.removeClass('show').addClass('hide');
+        }, 300);
+    }
+    else {
+        $body.addClass('modal-open');
+        $next.removeClass('hide').addClass('show');
+        setTimeout(function(){ 
+            $next.addClass('fade-in'); 
+        }, 100);
+    }
+});
+
+/*
+------------------------------------------------------------------------------------
+Close Web Work Modal
+------------------------------------------------------------------------------------
+ */
+$webWorkClose.on('click', function(e) {
+    var $this = $(this);
+    var $parentModal = $this.closest('.web-work-modal');
+
+    e.preventDefault();
+    
+    if ($parentModal.hasClass('show')) {
+        $body.removeClass('modal-open');
+        $parentModal.removeClass('fade-in'); 
+        setTimeout(function(){ 
+            $parentModal.removeClass('show').addClass('hide');
+        }, 300);
+    }
+});
 
 
-
-// Smooth Scroll Function
+/*
+------------------------------------------------------------------------------------
+Smooth Scroll Effect
+------------------------------------------------------------------------------------
+ */
 $("#nav a[href^='#']").on('click', function(e) {
-    navHeight = 50;
-
-   // prevent default anchor click behavior
-   e.preventDefault();
-   // store hash
-   var hash = this.hash;
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top - navHeight
-       }, 500, function(){
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
+    var navHeight = 50;
+    var hash = this.hash;
+    e.preventDefault();
+   
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top - navHeight}, 500, function(){
+        // when done, add hash to url (default click behaviour)
+        window.location.hash = hash;
     });
 });
 
-// Fade-in h2
+
+/*
+------------------------------------------------------------------------------------
+Fade in h2 in "About" section
+------------------------------------------------------------------------------------
+ */
 $(function() {
     $(window).scroll( function(){
     
